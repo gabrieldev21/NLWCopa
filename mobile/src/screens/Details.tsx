3a42e326e043160fd8bf6330b63303a6d2d53fff
gabrieldev21 <gabrielp.amorim@live.com>
2022-11-06 19:42:33 -0300

    first commit

---
+++ mobile/src/screens/Details.tsx
@@ -0,0 +1,10 @@
+import { VStack } from "native-base";
+import { Header } from "../components/Header";
+
+export function Details() {
+  return (
+    <VStack flex={1} bgColor="gray.900">
+      <Header title="Título do Bolão" showBackButton showShareButton />
+    </VStack>
+  );
+}
